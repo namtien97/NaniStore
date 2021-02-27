14d966baa1162672de4346818ba9c75c47a322b5
{NamTienNguyen} <{namtien97@gmail,com}>
2021-02-27 10:52:20 +0700

    configuration

---
+++ src/main/java/com/nanistore/demo/NaniStoreApplication.java
@@ -0,0 +1,12 @@
+package com.nanistore.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaniStoreApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(NaniStoreApplication.class, args);
+    }
+}
